55fe601fe2222690d14d1fb9a7f75d0b22cf51d8
IrsanJam <irsandjamaludin@gmail.com>
2024-02-07 06:46:36 +0700

    memperbarui fitur register

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages";
 import Login from "@/pages/auth/Login";
+import Register from "@/pages/auth/Register";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<>Not found</>} />
       </Routes>
     </BrowserRouter>
