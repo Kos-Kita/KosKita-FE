910d84e5c37ff2f2678e686d3aa7ff04c0f7d31a
IrsanJam <114846911+IrsanJam@users.noreply.github.com>
2024-02-08 13:56:41 +0700

    feat:  fitur create profile renter & owner (#15)
    * refactor: search menu
    
    * feat: memperbarui data
    
    * feat : components cardProduct
    
    * feat: profile owner

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -3,6 +3,8 @@ import Home from "../pages";
 import Login from "@/pages/auth/Login";
 import Register from "@/pages/auth/Register";
 import SearchMenu from "@/pages/Products/SearchMenu";
+import ProfileRenter from "@/pages/Profile/ProfileRenter";
+import ProfileOwner from "@/pages/Profile/ProfileOwner";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/searchmenu" element={<SearchMenu />} />
+        <Route path="/profilerenter" element={<ProfileRenter />} />
+        <Route path="/profileowner" element={<ProfileOwner />} />
         <Route path="*" element={<>Not found</>} />
       </Routes>
     </BrowserRouter>
