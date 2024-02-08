0f796e2fb61e2fe6505edc99d551053654be072b
IrsanJam <irsandjamaludin@gmail.com>
2024-02-08 13:46:42 +0700

    refactor: register

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 const Register = () => {
   const [searchParams, _setSearchParams] = useSearchParams();
   const tabParam = searchParams.get("tab");
+  const [showPassword, setPassword] = useState(true);
   const navigate = useNavigate();
 
   interface typeUser {
@@ -55,7 +56,7 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center py-8">
+    <div className="min-h-screen flex md:items-center md:justify-center md:py-8 bg-white">
       <div className="bg-white shadow flex flex-col items-center gap-5 max-w-md w-full p-3">
         <Tabs />
         <h2 className="text-3xl font-semibold">
@@ -67,7 +68,21 @@ const Register = () => {
             <input type="text" onChange={(e) => setUser((prev) => ({ ...prev, name: e.target.value }))} className="px-4 py-2 rounded-md bg-slate-100 w-full" required placeholder="Nama" value={user.name} />
             <input type="text" onChange={(e) => setUser((prev) => ({ ...prev, user_name: e.target.value }))} className="px-4 py-2 rounded-md bg-slate-100 w-full" required placeholder="Username" value={user.user_name} />
             <input type="email" onChange={(e) => setUser((prev) => ({ ...prev, email: e.target.value }))} className="px-4 py-2 rounded-md bg-slate-100 w-full" required placeholder="Email" value={user.email} />
-            <input type="password" onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))} className="px-4 py-2 rounded-md bg-slate-100 w-full" required placeholder="Password" value={user.password} />
+            <div className="flex  bg-slate-100 w-full gap-2">
+              <input
+                type={showPassword ? "password" : "text"}
+                onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))}
+                className=" bg-slate-100 px-4 py-2 rounded-md w-full"
+                required
+                placeholder="Password"
+                value={user.password}
+              />
+              {showPassword ? (
+                <img width="36px" onClick={() => setPassword(!showPassword)} height="16px" src="https://img.icons8.com/fluency-systems-regular/48/visible--v1.png" alt="visible--v1" className="p-2" />
+              ) : (
+                <img width="36px" onClick={() => setPassword(!showPassword)} height="16px" src="https://img.icons8.com/fluency-systems-filled/48/closed-eye.png" alt="closed-eye" className="p-2" />
+              )}
+            </div>
             <select onChange={(e) => setUser((prev) => ({ ...prev, gender: e.target.value }))} value={user.gender} className="p-3 w-full rounded-md bg-white border-[0.5px] border-slate-400">
               <option value="jenis-kelamin" className="ml-10">
                 Gender
@@ -78,7 +93,7 @@ const Register = () => {
           </div>
 
           <div className="flex justify-start items-start  text-base leading-5 text-neutral-900 max-md:flex-wrap max-md:max-w-full">
-            <div className="flex-auto gap-5 text-sm text-center items-start justify-start">
+            <div className="flex-auto gap-5 my-5 text-sm text-center items-start justify-start">
               <input type="checkbox" id="checkbox-register" required />
               <label htmlFor="checkbox-register"> Saya menyetujui Syarat dan Ketentuan, Kebijakan Privasi, dan menerima pembaruan melalui email.</label>
             </div>
