e16dbd07a8e63c50d2ebcbcae887f6943769f93a
IrsanJam <irsandjamaludin@gmail.com>
2024-02-18 07:54:08 +0700

    feat: regex username, name, email

--- src/pages/Profile/ProfileRenter.tsx
+++ src/pages/Profile/ProfileRenter.tsx
@@ -111,6 +111,26 @@ const ProfileRenter = () => {
     const gender = formData.gender;
     const email = formData.email;
     e.preventDefault();
+
+    const specialCharsRegex = /[^a-zA-Z0-9_]+/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,3}$/;
+
+    if (specialCharsRegex.test(name) || specialCharsRegex.test(user_name)) {
+      toast({
+        variant: "destructive",
+        description: "Name dan User Name hanya boleh berisi huruf, angka, dan underscore",
+      });
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      toast({
+        variant: "destructive",
+        description: "Format email tidak valid. Email harus berakhir dengan .com, .net, .org, atau ekstensi domain lainnya yang berukuran 2 atau 3 karakter.",
+      });
+      return;
+    }
+
     try {
       const formData = new FormData();
       if (selectedImage) {
@@ -381,7 +401,7 @@ const ProfileRenter = () => {
                           onChange={handlePerubahan}
                           value={formData.email}
                           required
-                          type="text"
+                          type="email"
                           placeholder="Masukan Email"
                           className="grow  focus:outline-none w-[45vw] md:w-[22vw] p-4 bg-white rounded border border-solid shadow-sm border-zinc-400 text-zinc-900 max-md:pr-5"
                         />
